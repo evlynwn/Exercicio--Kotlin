d6d5c647329c32fbc7dabf6c7702211789d25ff2
EVELLYN SANTIAGO DE SOUZA <25203674@dsn127m1249386.cfp127aluno.local>
2026-08-27 16:57:15 -0300

    finalizando exercicios

--- src/Main.kt
+++ src/Main.kt
@@ -2,10 +2,20 @@ fun main() {
 
     val numero = 3
     println(par(numero))
-    val idade = 17
+    val idade = 60
     idade(idade)
     val numeros = 89
     numeros(numeros)
+    val tabuada = 4
+    tabuada(tabuada)
+    val soma = 5
+    soma(soma)
+    val lista = 2
+    lista(lista)
+    val listaNumeros = 10
+    listaNumeros(listaNumeros)
+    val dobro = 5
+    dobro(dobro)
 
 }
 
@@ -23,20 +33,20 @@ fun par(numero:Int): String{
 }
 fun numeros ( positNegZer : Int){
 
-    val numeros = 89
+    val numeros = -89
 
     when  {
-        numeros >= 0 -> println("Zero")
-        numeros == 0 -> println("Negativo")
-        else -> println("Positivo")
+        numeros > 0 -> println("Positivo")
+        numeros < 0 -> println("Negativo")
+        else -> println("Zero")
 
     }
 
 }
 
-fun idade ( caai: Int){
+fun idade ( classificacao: Int){
 
-    val idade = 17
+    val idade = 60
 
     when (idade) {
         in 0 .. 12 -> println("Crianca")
@@ -46,3 +56,59 @@ fun idade ( caai: Int){
     }
 
 }
+fun tabuada( divisao: Int){
+    var numeroEscolhido = 4
+
+}
+fun soma (somas: Int){
+
+    var soma = arrayOf(1,2,3,4,5).toList()
+
+    println(soma - 2)
+
+}
+fun lista (valor: Int){
+
+    val lista = arrayOf(1,2,10,3)
+
+    var maiorNumero = lista[0]
+
+    for (numero in lista){
+        if (maiorNumero < numero){
+            maiorNumero = numero
+        }
+    }
+    println(maiorNumero)
+
+}
+fun listaNumeros( numero: Int) {
+    var i = 10
+
+    while ( i >= 0 ) {
+
+        i--
+        println(i)
+    }
+}
+fun pares ( soma: Int) {
+    val somaPares = arrayOf(1,2,3,4,5)
+
+    val par = 0
+
+}
+fun dobro (valor : Int) {
+
+    val dobro = 5
+    println( dobro * 2)
+}
+fun media () {
+    val nota1 = 3
+    val nota2 = 6
+    val nota = 7
+
+
+
+    println()
+
+}
+
